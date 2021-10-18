e02ce2dfecd7fec1eaacd0014c5c77e3831bb159
miracle <jordanmiracle@protonmail.com>
2021-10-18 15:54:23 -0700

    the panels are the correct size and scale dynamically when ading elements to the db.

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, primary_key=True)
     description = models.CharField(max_length=500)
     url = models.URLField(blank=True)
     code_url = models.URLField(blank=True)
