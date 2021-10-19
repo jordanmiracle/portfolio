be3752f7c03aadfa3fa74b9a083dce1603147f7f
miracle <jordanmiracle@protonmail.com>
2021-10-18 23:53:21 -0700

    remade migrations, changed the models slightly so that the id would work and I did not get the default field error.

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100, primary_key=True)
-    description = models.CharField(max_length=500)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
     url = models.URLField(blank=True)
     code_url = models.URLField(blank=True)
-    image = models.ImageField(null=False, blank=False, upload_to='static/portfolioapp/images')
+    image = models.ImageField(null=True, blank=True, upload_to='static/portfolioapp/images')
 
     def __str__(self):
         return self.title
