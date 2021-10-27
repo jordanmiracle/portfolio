50ccf31acb68d872d28965975b72915933faaed5
miracle <jordanmiracle@protonmail.com>
2021-10-27 16:05:22 -0700

    staticfiles.json added and added some loggin and debug messages to settings

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
