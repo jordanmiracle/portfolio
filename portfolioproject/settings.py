1c75a177aea791bc765267ca820600910de5e20b
miracle <jordanmiracle@protonmail.com>
2021-10-27 16:25:42 -0700

    made static root "static" instead of staticfiles

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -176,7 +176,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
