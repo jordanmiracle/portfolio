aebfd85c8a27b4e11f6d6cadadc7f3d2837566d3
miracle <jordanmiracle@protonmail.com>
2021-10-26 23:35:18 -0700

    fixed import

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import handler400, handler500
-from . import views
+from article import views
 
 app_name = "article"
 
