1591f08df3368cee9175621b61e8897e9bb107ef
miracle <jordanmiracle@protonmail.com>
2021-10-26 23:17:33 -0700

    added things necessary to push to heroku.

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -19,6 +19,8 @@ from django.contrib import staticfiles, postgres
 import boto3
 import base64
 from botocore.exceptions import ClientError
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,10 +31,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-imhco(wv(_uvii2@%cyf4fpak^8w=4a7yi_6=)20pp%oaykm)d'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'thejordanmiracle.com', 'thejordanmiracle.herokuapp.com']
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 60
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'jordanmiracle.com', 'https://www.jordanmiracle.com']
 
 # Application definition
 
@@ -46,6 +66,8 @@ INSTALLED_APPS = [
     'portfolioapp',
     'article',
     'crispy_forms',
+    'storages',
+    'boto3',
 ]
 
 MIDDLEWARE = [
@@ -86,7 +108,7 @@ FIXTURES = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #   'default': {
 #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #       'USER': 'jordanmiracle',
@@ -95,7 +117,7 @@ FIXTURES = [
 #       'PASSWORD': 'Likeacarrot23!',
 #       'PORT': '5432',
 #   },
-#}
+# }
 
 # DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
@@ -141,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
@@ -151,8 +173,8 @@ STATICFILES = [
     BASE_DIR / 'static'
 ]
 
-#MEDIA_ROOT = BASE_DIR / 'static/images'
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
+# MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -160,3 +182,79 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+###### AWS Settings  ######
+
+def get_secret():
+    secret_name = "gcet-bucket"
+    region_name = "eu-west-3"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'jordanmiraclebucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_PRELOAD_METADATA = True
+COMPRESS_OFFLINE = True
+COMPRESS_ENABLED = True
+AWS_MEDIA_LOCATION = 'media'
+AWS_PUBLIC_LOCATION = 'public'
+PRIVATE_FILE_STORAGE = 'portfolioproject.storage_backends.MediaStorage'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_LOCATION = 'static'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+PUBLIC_MEDIA_LOCATION = 'media'
+# MEDIA_URL = f'//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME  # This was changed after we got everything up and running again
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
---
+++ portfolioproject/storages_backends.py
@@ -0,0 +1,15 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+    default_acl = 'public-read'
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.AWS_MEDIA_LOCATION
+    default_acl = 'public-read'
+    file_overwrite = False
+    custom_domain = False
