881e93108605f73b6c2751f67b8b9b832379e5da
miracle <jordanmiracle@protonmail.com>
2021-10-27 15:59:10 -0700

    staticfiles.json added and added some loggin and debug messages to settings

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -47,6 +47,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+
 SITE_ID = 1
 
 if not DEBUG:
@@ -249,3 +252,40 @@ if not DEBUG:
     DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 django_heroku.settings(locals())
+
+
+
+### Logging ###
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
