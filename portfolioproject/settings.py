e2ce5a683b409b36822963583102ee38d9c6b267
miracle <jordanmiracle@protonmail.com>
2021-10-28 02:23:30 -0700

    changed s3 again. No clue why it is not working after several time sof using the same settings.

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -251,10 +251,10 @@ def get_secret():
 
 
 AWS_STORAGE_BUCKET_NAME = 'jordanmiraclebucket'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 #AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
@@ -264,13 +264,14 @@ AWS_S3_OBJECT_PARAMETERS = {
 #AWS_PUBLIC_LOCATION = 'public'
 #PRIVATE_FILE_STORAGE = 'portfolioproject.storage_backends.MediaStorage'
 #AWS_S3_REGION_NAME = 'us-east-1'
-#AWS_LOCATION = 'static'
+AWS_LOCATION = ''
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/' % AWS_LOCATION
 #MEDIAFILES_LOCATION = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 # PUBLIC_MEDIA_LOCATION = 'media'
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 if not DEBUG:
     django_heroku.settings(locals(), staticfiles=False)
