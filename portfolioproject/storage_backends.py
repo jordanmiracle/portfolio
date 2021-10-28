985c5d4ad91e1a3a3fc0696b3355af924e10b66c
miracle <jordanmiracle@protonmail.com>
2021-10-28 04:01:49 -0700

    storage backends

--- portfolioproject/storage_backends.py
+++ portfolioproject/storage_backends.py
@@ -1,13 +1,13 @@
-#from storages.backends.s3boto3 import S3Boto3Storage
-#from django.conf import settings
-#
-#
-#class PublicMediaStorage(S3Boto3Storage):
-#    location = 'media'
-#    file_overwrite = False
-#    default_acl = 'public-read'
-#
-#
-#class MediaStorage(S3Boto3Storage):
-#    location = settings.AWS_MEDIA_LOCATION
-#    file_overwrite = False
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
+    default_acl = 'public-read'
+
+
+class MediaStorage(S3Boto3Storage):
+    location = settings.AWS_MEDIA_LOCATION
+    file_overwrite = False
