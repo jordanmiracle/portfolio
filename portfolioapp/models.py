85e653bb6506bea3c52e370f060bfa69479e6c26
miracle <jordanmiracle@protonmail.com>
2021-10-28 03:17:42 -0700

    changed the models for image so that storage=MediaStorage, and added some extra urlpatterns to the app urls

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from portfolioproject.storage_backends import MediaStorage, PublicMediaStorage
 
 class Project(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
-    description = models.CharField(max_length=500, blank=True, null=True)
+    description = models.CharField(max_length=750, blank=True, null=True)
     url = models.URLField(blank=True)
     code_url = models.URLField(blank=True)
-    image = models.ImageField(null=True, blank=True, upload_to='static/portfolioapp/images')
+    image = models.ImageField(null=True, blank=True, storage=MediaStorage)
 
     def __str__(self):
         return self.title
--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from portfolioapp import views
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
