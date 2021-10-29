29fa4cbd1ef1447586b9e2bfbe92697ec83937fe
miracle <jordanmiracle@protonmail.com>
2021-10-28 18:04:38 -0700

    removed ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) from urls and added .url to {{ project.image }}

--- portfolioproject/urls.py
+++ portfolioproject/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
         path('<int:id>/', views.project_detail, name='project_detail'),
         path('', include('portfolioapp.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
