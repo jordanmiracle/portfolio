3f01ac3382543ceadc655a73dfc15d15cfd0b0ba
miracle <jordanmiracle@protonmail.com>
2021-11-01 17:19:04 -0700

    Inlined the stars js script and am lazy loading the images on the project cards.

--- portfolioapp/static/portfolioapp/js/main.js
+++ portfolioapp/static/portfolioapp/js/main.js
@@ -204,30 +204,7 @@
     }
 
 })(jQuery);
-(() => {
-    const max_stars = 250;
-    const stars = [];
 
-    for (let i = 0; i < max_stars; i++) {
-        const star = document.createElement('span');
-        const size = (Math.floor(Math.random() * 4) + 1);
-        star.className = 'star';
-        star.style.width = size + 'px';
-        star.style.height = size + 'px';
-        star.style.background = `rgba(255, 255, 177, ${Math.random()})`;
-        star.style.top = Math.ceil(Math.random() * 100) + '%';
-        star.style.left = Math.ceil(Math.random() * 100) + '%';
-        stars.push(star);
-        document.body.appendChild(star);
-    }
-
-    for (let j = 0; j < max_stars * 0.6; j++) {
-        const star = stars[j];
-        star.style.animationName = 'glow';
-        star.style.animationDelay = (Math.floor(Math.random() * 6) + 1) + 's';
-        star.style.animationDuration = (Math.floor(Math.random() * 6) + 1) + 's';
-    }
-})();
 (function () {
     const tab = document.querySelector('.card');
 
